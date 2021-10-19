de73271d6de65fe6056c3f88c878f938658cf777
Abdelhadi Elouarguli <69119385+itsabdelll@users.noreply.github.com>
2021-10-19 13:41:31 +0100

    Add files via upload

---
+++ c05/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abel-oua <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/13 16:38:43 by abel-oua          #+#    #+#             */
+/*   Updated: 2021/10/13 16:38:49 by abel-oua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power >= 1)
+		return (nb * ft_recursive_power(nb, (power - 1)));
+	return (nb);
+}
